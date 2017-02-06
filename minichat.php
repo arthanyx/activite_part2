e863971589751bff7cd50504d9fb2bb0ffc115b6
arthanyx <arthanyx@users.noreply.github.com>
2017-02-06 13:03:16 +0100

    Suppression des commentaires

--- minichat.php
+++ minichat.php
@@ -22,7 +22,6 @@
     </form>
 
 <?php
-// Connexion à la base de données
 try
 {
 	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
@@ -32,10 +31,8 @@ catch(Exception $e)
         die('Erreur : '.$e->getMessage());
 }
 
-// Récupération des 10 derniers messages
 $reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
 
-// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
 while ($donnees = $reponse->fetch())
 {
 	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
